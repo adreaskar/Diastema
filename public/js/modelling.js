4e07d10d6b764386e331ab9a7121d70f0bbe0bdf
Andreas Karabetian <adreas@karabetian.gr>
2022-02-04 15:10:44 +0200

    removed nodes from playbook

--- public/js/modelling.js
+++ public/js/modelling.js
@@ -182,14 +182,14 @@ $(document).ready(function() {
 				"analysis-date": ('0'+d.getDate()).slice(-2) + "-" + ('0'+(d.getMonth()+1)).slice(-2) + "-" + d.getFullYear(), 
 				"analysis-time": ('0'+d.getHours()).slice(-2) + ":" + ('0'+d.getMinutes()).slice(-2) + ":" + ('0'+d.getSeconds()).slice(-2) + ":" + d.getMilliseconds()
 			},
-			//"nodes":[],
+			"nodes":[],
 			"connections":[]
 		};
 
-		// Get nodes ------------------- NODES WILL RETURN FOR DEMO 2
-		// for (l in diagram) {
-		// 	data.nodes.push(diagram[l]);
-		// }
+		// Get nodes -------------------
+		for (l in diagram) {
+			data.nodes.push(diagram[l]);
+		}
 		// Get connections ----------------------
 		for (k in linesArray) {
 			data.connections.push(linesArray[k]);
@@ -262,6 +262,8 @@ $(document).ready(function() {
 		} else {
 			data.automodel = false;
 		}
+
+		delete data.nodes; // NODES WILL RETURN IN DEMO 2
 	}
 
 	// Load data
